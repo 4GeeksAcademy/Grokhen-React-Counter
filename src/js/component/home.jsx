a5c5f14b3ac89f5fccdb70ee3ccac8f80afb9554
Grokhen <152092079+Grokhen@users.noreply.github.com>
2024-02-20 17:57:00 +0000

    layout correction

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,35 +7,23 @@ const Home = ({ value }) => {
 	return (
 		<div className="container text-center">
 			<div className="row card-group justify-content-center ">
-				<div className="col card text-bg-dark" >
-					<div className="card-body">
-						<h2 className="card-text fs-1"><i className="far fa-clock"></i></h2>
-					</div>
+				<div className="col-1 text-bg-dark" >
+					<h2 className="fs-1"><i className="far fa-clock"></i></h2>
 				</div>
-				<div className="col card text-bg-dark" >
-					<div className="card-body">
-						<h2 className="card-text fs-1">{(Math.floor(value / 10000)) % 10}</h2>
-					</div>
+				<div className="col-1 text-bg-dark" >
+					<h2 className=" fs-1">{(Math.floor(value / 10000)) % 10}</h2>
 				</div>
-				<div className="col card text-bg-dark" >
-					<div className="card-body">
-						<h2 className="card-text fs-1">{(Math.floor(value / 1000)) % 10}</h2>
-					</div>
+				<div className="col-1  text-bg-dark" >
+					<h2 className=" fs-1">{(Math.floor(value / 1000)) % 10}</h2>
 				</div>
-				<div className="col card text-bg-dark" >
-					<div className="card-body">
-						<h2 className="card-text fs-1">{(Math.floor(value / 100)) % 10}</h2>
-					</div>
+				<div className="col-1 text-bg-dark" >
+					<h2 className=" fs-1">{(Math.floor(value / 100)) % 10}</h2>
 				</div>
-				<div className="col card text-bg-dark" >
-					<div className="card-body">
-						<h2 className="card-text fs-1">{(Math.floor(value / 10)) % 10}</h2>
-					</div>
+				<div className="col-1 text-bg-dark" >
+					<h2 className=" fs-1">{(Math.floor(value / 10)) % 10}</h2>
 				</div>
-				<div className="col card text-bg-dark" >
-					<div className="card-body">
-						<h2 className="card-text fs-1">{(Math.floor(value / 1)) % 10}</h2>
-					</div>
+				<div className="col-1 text-bg-dark" >
+					<h2 className=" fs-1">{(Math.floor(value / 1)) % 10}</h2>
 				</div>
 			</div>
 		</div>
